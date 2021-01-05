3cfdb97593d2157d63e5740d66884aa7a6830c85
Paula <druetta.paula@gmail.com>
2021-01-05 00:35:20 -0300

    ADD: burger menu lateral

--- stock/static/js/script.js
+++ stock/static/js/script.js
@@ -1,3 +1,19 @@
+let hamburger = document.getElementById("logoMenu");
+let menu = document.getElementById("menu");
+let main = document.getElementsByTagName("main")[0];
+hamburger.addEventListener("click", mostrarMenu);
+main.addEventListener("click", cerrarMenu);
+
+function mostrarMenu () {
+  menu.classList.toggle('hidden');
+}
+
+function cerrarMenu () {
+  if (!(menu.classList.contains('hidden'))){
+    menu.classList.toggle('hidden');
+  }
+}
+
 let recetas;
 recetas =
 [{
